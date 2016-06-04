d335d4716684b65e0ec0a0824625d8fdb3de7775
breiss <brad.p.reiss@gmail.com>
2016-06-04 13:34:24 +0000

    created method to sort rss entries by date

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
     has_many :feeds
     has_secure_password
     
+    def sort_feeds
+        arr=[]
+        self.feeds.all.each do |feed|
+            feed = RSS::Parser.parse(feed.url)
+            feed.items.each do |item| 
+                arr << {date: item.pubDate, title: item.title, link: item.link, channel: feed.channel.title, summary: item.description}
+            end
+        end
+        arr.sort_by {|x| x[:date]}.reverse
+    end
+    
 end
