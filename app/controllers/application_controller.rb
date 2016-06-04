92ce741070942b4431e08a82879e2ed38f5aa204
breiss <brad.p.reiss@gmail.com>
2016-06-04 11:11:33 +0000

    added bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,8 +50,9 @@ class ApplicationController < Sinatra::Base
 	#*************FEEDS***************#
 
 	get "/feeds" do
+	  #@message = params[:message] if params.has_key?(:message) 
 	  #url = 'http://bits.blogs.nytimes.com/feed/'
-	  @feeds = current_user.feeds #RSS::Parser.parse(open(url))
+	  @feeds = current_user.feeds
 	  erb :"feeds/index"
 	end
 	
@@ -63,11 +64,11 @@ class ApplicationController < Sinatra::Base
 	post "/feeds/manage" do
 		if RSS::Parser.parse(params[:url]).nil?
 			puts "Invalid RSS feed" 
-			redirect '/feeds'
+			redirect '/feeds/manage?message=The RSS feed is not valid.'
 		end
 		current_user.feeds << Feed.new(url: params[:url]) 
 		current_user.save
-		redirect '/feeds'
+		redirect '/feeds/manage?message=Successfully added the RSS feed!'
 	end
 	
 	post "/feeds/:id/delete" do
